fb4196cb95692778b677eda0f2c12f353ed8cee6
invelop <webfenglin@163.com>
2019-08-29 11:10:12 +0800

    Added new features

--- Plugin.php
+++ Plugin.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * <a href='https://blog.irow.top/archives/387.html' title='mainpage' target='_blank'>百度主动提交工具</a>
+ * <a href='https://blog.irow.top/archives/562.html' title='mainpage' target='_blank'>百度主动提交工具</a>
  *
  * @package ActiveSubmit
  * @author 承影
- * @version 0.2
+ * @version 1.0
  * @link https://blog.irow.top/
  */
 
@@ -14,7 +14,10 @@ class ActiveSubmit_Plugin implements Typecho_Plugin_Interface
     /* 激活插件方法 */
     public static function activate()
     {
-      Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array('ActiveSubmit_Plugin', 'render');
+      Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array('ActiveSubmit_Plugin', 'submit');
+      Typecho_Plugin::factory('Widget_Contents_Page_Edit')->finishPublish = array('ActiveSubmit_Plugin', 'submit');
+      Typecho_Plugin::factory('Widget_Contents_Post_Edit')->delete = array('ActiveSubmit_Plugin', 'del');
+      Typecho_Plugin::factory('Widget_Contents_Page_Edit')->delete = array('ActiveSubmit_Plugin', 'del');
     }
 
     /* 禁用插件方法 */
@@ -23,66 +26,53 @@ class ActiveSubmit_Plugin implements Typecho_Plugin_Interface
     /* 插件配置方法 */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
-      $api = new Typecho_Widget_Helper_Form_Element_Text(
-        'api', NULL, '',
-        _t('百度推送接口：'),
-        _t('如果不知道自己的百度推送接口，请到<a href=\'https://ziyuan.baidu.com/linksubmit/\' title=\'百度资源搜索平台\' target=\'_blank\'>百度搜索资源平台</a>获取')
+      $token = new Typecho_Widget_Helper_Form_Element_Text(
+        'token', NULL, '',
+        _t('百度推送接口token：'),
+        _t('如果不知道自己的百度推送接口token，请到<a href=\'https://ziyuan.baidu.com/linksubmit/\' title=\'百度资源搜索平台\' target=\'_blank\'>百度搜索资源平台</a>获取')
       );
-      $form->addInput($api);
+      $form->addInput($token);
+      $error = new Typecho_Widget_Helper_Form_Element_Radio('error',
+          ['0' => _t('否'), '1' => _t('是')],
+          '0', _t('是否开启错误日志'), _t('开启后在插件目录下会生成error.log记录错误(需要插件目录可写权限)'));
+      $form->addInput($error);
     }
 
     /* 个人用户的配置方法 */
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
     /* 插件实现方法 */
-    public static function render($contents, $class){
-      $options = Helper::options();
+    public static function render(){}
+
+    public static function submit($contents, $edit)
+    {
+      $url = $edit->permalink;
+
+      if (is_null(Helper::options()->plugin('ActiveSubmit')->token)) {
+          throw new Typecho_Plugin_Exception(_t('参数未正确配置，自动提交失败'));
+      }else{
+          $token = Helper::options()->plugin('ActiveSubmit')->token;
+      }
+
       //确认是否发布
       if ('publish' != $contents['visibility'] || $contents['created'] > time()) {
           return;
       }
-      //判断是否配置相关信息
-      if (is_null($options->plugin('ActiveSubmit')->api)) {
-          throw new Typecho_Plugin_Exception(_t('参数未正确配置，自动提交失败'));
-      }else{
-          $api = $options->plugin('ActiveSubmit')->api;
-      }
       //一天之内的文章推送
-      if((int)$article['created']+86400 < (int)$article['modified'] ){//之前判断忽略了自动保存草稿的问题
+      if((int)$article['created']+86400 < (int)$article['modified'] ){
           return;
       }
-      //构造链接
-      $post = array('cid' => $class->cid);
-      $options = Helper::options();
-      $db = Typecho_Db::get();
 
-      $routeExists = (NULL != Typecho_Router::get('post'));
-      if(!is_null($routeExists)){
-          $post['categories'] = $db->fetchAll($db->select()->from('table.metas')
-                  ->join('table.relationships', 'table.relationships.mid = table.metas.mid')
-                  ->where('table.relationships.cid = ?', $post['cid'])
-                  ->where('table.metas.type = ?', 'category')
-                  ->order('table.metas.order', Typecho_Db::SORT_ASC));
+	    $baseurl = str_replace("/index.php", "", Helper::options()->index);
 
-          $post['category'] = urlencode(current(Typecho_Common::arrayFlatten($post['categories'], 'slug')));
-          $post['directory']=$post['category'];
-          foreach ($post['categories'] as $category) {
-              if(0!=$category['parent']){
-                  $parent = $db->fetchRow($db->select()->from('table.metas')
-                      ->where('table.metas.mid = ?',$category['parent']));
-                  $post['directory'] = urlencode($parent['slug']).'/'.urlencode($category['slug']);
-                  break;
-              }
-          }
-          $post['slug'] = urlencode($post['slug']);
-          $post['date'] = new Typecho_Date($post['created']);
-          $post['year'] = $post['date']->year;
-          $post['month'] = $post['date']->month;
-          $post['day'] = $post['date']->day;
+      $method = 'urls';
+
+      //更新文章
+      if((int)$article['created']!=(int)$article['modified'] ){
+        $method = 'update';
       }
 
-      $post['pathinfo'] = $routeExists ? Typecho_Router::url('post', $post) : '#';
-      $url = Typecho_Common::url($post['pathinfo'], $options->index);
+      $api = "http://data.zz.baidu.com/update?site={$baseurl}&token={$token}";
 
       //主动推送
       $ch = curl_init();
@@ -97,19 +87,111 @@ class ActiveSubmit_Plugin implements Typecho_Plugin_Interface
       $result = curl_exec($ch);
       curl_close($ch);
 
-      /*错误日志
-      $route = __TYPECHO_ROOT_DIR__.'/usr/plugins/ActiveSubmit/'
-      $file = $route.'error.log';
-      if($result){
-        $result = json_decode($result);
-        if(1!=$result['success']){
-          file_put_contents($file, json_encode($result)."\n", FILE_APPEND);
+      /*错误日志*/
+      if(Helper::options()->plugin('ActiveSubmit')->error){
+        $route = __TYPECHO_ROOT_DIR__.'/usr/plugins/ActiveSubmit/';
+        $file = $route.'error.log';
+        if($result){
+          $result = json_decode($result, $assoc=true);
+          if(1!=$result['success']){
+            file_put_contents($file, date('Y-m-d-H:i:s')."\t{$method}\t".json_encode($result)."\n", FILE_APPEND);
+          }
+        }
+        else{
+          file_put_contents($file, date('Y-m-d-H:i:s')."\t{$method}\t".json_encode($result)."\n", FILE_APPEND);
         }
       }
-      else{
-        file_put_contents($file, json_encode($result)."\n", FILE_APPEND);
+
+    }
+
+
+    public static function del($cid, $edit){
+
+      $url = self::get_url($cid);
+
+      //判断是否配置相关信息
+      if (is_null(Helper::options()->plugin('ActiveSubmit')->token)) {
+          throw new Typecho_Plugin_Exception(_t('参数未正确配置，自动提交失败'));
+      }else{
+          $token = Helper::options()->plugin('ActiveSubmit')->token;
       }
-      */
+
+      $baseurl = str_replace("/index.php", "", Helper::options()->index);
+
+      $api = "http://data.zz.baidu.com/del?site={$baseurl}&token={$token}";
+
+      //主动推送
+      $ch = curl_init();
+      $options =  array(
+          CURLOPT_URL => $api,
+          CURLOPT_POST => true,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_POSTFIELDS => $url,
+          CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+      );
+      curl_setopt_array($ch, $options);
+      $result = curl_exec($ch);
+      curl_close($ch);
+
+      /*错误日志*/
+      if(Helper::options()->plugin('ActiveSubmit')->error){
+        $route = __TYPECHO_ROOT_DIR__.'/usr/plugins/ActiveSubmit/';
+        $file = $route.'error.log';
+        if($result){
+          $result = json_decode($result, $assoc=true);
+          if(1!=$result['success']){
+            file_put_contents($file, date('Y-m-d-H:i:s')."\tdelete\t".json_encode($result)."\n", FILE_APPEND);
+          }
+        }
+        else{
+          file_put_contents($file, date('Y-m-d-H:i:s')."\tdelete\t".json_encode($result)."\n", FILE_APPEND);
+        }
+      }
+    }
+
+    public static function get_url($cid)
+    {
+      $db = Typecho_Db::get();
+      $value = array();
+      /** 取出所有分类 */
+      $value['cid'] = $cid;
+      $value['type'] = $db->fetchRow($db->select('type')->from('table.contents')->where('table.contents.cid = ?', $value['cid']))['type'];
+      $value['categories'] = $db->fetchAll($db
+          ->select()->from('table.metas')
+          ->join('table.relationships', 'table.relationships.mid = table.metas.mid')
+          ->where('table.relationships.cid = ?', $value['cid'])
+          ->where('table.metas.type = ?', 'category')
+          ->order('table.metas.order', Typecho_Db::SORT_ASC));
+      $value['category'] = NULL;
+      $value['directory'] = array();
+
+      /** 取出第一个分类作为slug条件 */
+      if (!empty($value['categories'])) {
+          $value['category'] = $value['categories'][0]['slug'];
+
+          $value['directory'] = Typecho_Widget::widget('Widget_Metas_Category_List')->getAllParentsSlug($value['categories'][0]['mid']);
+          $value['directory'][] = $value['category'];
+      }
+
+
+      /** 生成访问权限 */
+      $value['hidden'] = false;
+
+      /** 获取路由类型并判断此类型在路由表中是否存在 */
+      $type = $value['type'];
+      $routeExists = (NULL != Typecho_Router::get($type));
+
+      $value['slug'] = urlencode($value['slug']);
+      $value['category'] = urlencode($value['category']);
+      $value['directory'] = implode('/', array_map('urlencode', $value['directory']));
+
+      /** 生成静态路径 */
+      $value['pathinfo'] = $routeExists ? Typecho_Router::url($type, $value) : '#';
+
+      /** 生成静态链接 */
+      $value['permalink'] = Typecho_Common::url($value['pathinfo'], Helper::options()->index);
+
+      return $value['permalink'];
     }
 
 }
--- README.md
+++ README.md
@@ -7,7 +7,11 @@
 
 ## 食用说明
 
-激活后在后台配置你的百度推送接口（在百度搜索资源平台-提交链接处获取，需要有百度账号并站点所有权验证）
+- 上传文件后解压并重命名为ActiveSubmit
+- 激活后在后台配置你的百度推送接口**token**（在百度搜索资源平台-提交链接处获取，需要有百度账号并站点所有权验证）
+- 选择是否生成错误日志（如果生成，需要插件目录下可写权限）
+
+![接口地址](https://blog.irow.top/usr/uploads/2019/07/2349109388.png)
 
 
 
@@ -17,4 +21,4 @@ Github：<https://github.com/invelop/Typecho-ActiveSubmit>
 
 码云：<https://gitee.com/ETAS/Typecho-ActiveSubmit>
 
-博客：<https://blog.irow.top/archives/387.html>
+博客：<https://blog.irow.top/archives/562.html>
